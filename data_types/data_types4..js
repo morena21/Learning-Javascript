25632d5f0771df395503497cbac288bcaf95b90a
morena21 <morena.02.farinelli@gmail.com>
2021-04-15 09:52:34 +0200

    quarto

---
+++ data_types/data_types4..js
@@ -0,0 +1,7 @@
+
+console.log(typeof "" + 
+typeof "sos"  + 
+typeof "John Doe"+
+typeof 34 +
+typeof [1,2,3,4] );
+
---
+++ data_types/data_types5.js
@@ -0,0 +1,6 @@
+var p1=25;
+var p2=25;
+var r=0;
+function myFunction(p1, p2) {
+    return r=p1 * p2;
+  }    
