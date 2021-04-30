0ec4031851f736148410b271f9bca21ffefa8a02
morena21 <morena.02.farinelli@gmail.com>
2021-04-30 22:21:37 +0200

    undicisemo

---
+++ scope/scope2.js
@@ -0,0 +1,34 @@
+hello = function() {
+    return "Hello World!";
+  }
+
+  hello = () => {                               // VIENE UTILIZZATA LA FRECCIA PER ACCORCIARE
+     return "Hello World!";
+  }
+
+
+  hello = () => "Hello World!"; //Se la funzione ha una sola istruzione e l'istruzione restituisce un valore, è possibile rimuovere le parentesi e la return
+
+
+var arr2=[2,3,4,5,6,7,8,9];
+
+    for (var j = 0 ; j < arr2.length ; j++){
+        if(arr2[j]<= 4){
+            var quad = arr2[j]*arr2[j];
+            console.log(quad);
+        }
+        else{
+            console.log("termina elaborazione");
+        }
+    }
+    
+
+
+var arr1 = ["giovanni", "mario" , 34, 100, "ciao", 45];    // DA FINIRE
+console.log(parseInt.arr1);
+function filtrare(arr1){
+    for(var i= 0 ; i < arr1.length ; i++){
+
+    }
+}
+
